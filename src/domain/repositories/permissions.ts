400c5b984da25c604d2a3fa56b337eaf36f20e22
zeworks <josenogueira95@hotmail.com>
2022-09-28 02:46:02 +0100

    setup permissions repository interface

--- src/domain/repositories/permissions.ts
+++ src/domain/repositories/permissions.ts
@@ -1,5 +1,13 @@
 import { CreatePermissionUseCase } from "../usecases/permissions/create-permission";
+import { DeletePermissionUseCase } from "../usecases/permissions/delete-permission";
+import { GetAllPermissionsUseCase } from "../usecases/permissions/get-all-permissions";
+import { GetPermissionByIdUseCase } from "../usecases/permissions/get-permission";
+import { UpdatePermissionUseCase } from "../usecases/permissions/update-permission";
 
 export interface PermissionsRepository {
-  create: CreatePermissionUseCase
+  create: CreatePermissionUseCase,
+  update: UpdatePermissionUseCase,
+  delete: DeletePermissionUseCase,
+  getById: GetPermissionByIdUseCase,
+  getAll: GetAllPermissionsUseCase
 }
