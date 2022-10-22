4a79a19f2bb859b242372d1f504c0dcffffe969a
José Nogueira <josenogueira95@hotmail.com>
2022-10-22 03:15:06 +0100

    #ORI-80 | Create Company + Create Contact (use case + repository) (#13)

---
+++ src/data/usecases/companies/db-create-company.spec.ts
@@ -0,0 +1,18 @@
+import { InMemoryCompaniesRepository } from "@/data/protocols/repositories/companies/in-memory-companies-repository";
+import { UuidAdapter } from "@/infra/cryptography/uuid";
+import { expect, test } from "vitest";
+import { DbCreateCompany } from "./db-create-company";
+
+test("Should create company with success", async () => {
+  const uuidAdapter = new UuidAdapter();
+  const companiesRepository = new InMemoryCompaniesRepository();
+  const createCompany = new DbCreateCompany(uuidAdapter, companiesRepository);
+
+  const result = await createCompany.create({
+    code: "code-1",
+    name: "company name",
+  })
+
+  expect(result?.name).toEqual("company name");
+  expect(result?.status).toEqual(true);
+})
