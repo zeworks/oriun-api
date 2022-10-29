c765378893f4fae60bc277e641cd6ff6d627f5a7
José Nogueira <josenogueira95@hotmail.com>
2022-10-29 19:12:42 +0100

    #ORI-109 | add load company resolver (#22)

--- src/main/apollo/resolvers/companies.ts
+++ src/main/apollo/resolvers/companies.ts
@@ -3,10 +3,12 @@ import { apolloControllerAdapter } from "@/main/adapters/apollo-controller";
 import { AclDecorator } from "@/main/decorators/acl-decorator";
 import { makeCreateCompanyController } from "@/main/factories/controllers/companies/create-company-controller-factory";
 import { makeLoadCompaniesController } from "@/main/factories/controllers/companies/load-companies-controller-factory";
+import { makeLoadCompanyByIdController } from "@/main/factories/controllers/companies/load-company-by-id-controller-factory";
 
 export default {
   Query: {
     companies: (_: any, args: any, context: any) => apolloControllerAdapter(new AclDecorator(makeLoadCompaniesController(), PermissionKey.CompaniesView), args, context),
+    company: (_: any, args: any, context: any) => apolloControllerAdapter(new AclDecorator(makeLoadCompanyByIdController(), PermissionKey.CompaniesView), args, context),
   },
   Mutation: {
     createCompany: (_: any, args: any, context: any) => apolloControllerAdapter(new AclDecorator(makeCreateCompanyController(), PermissionKey.CompaniesCreate), args.input, context),
