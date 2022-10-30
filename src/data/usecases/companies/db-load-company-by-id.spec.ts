4fe7e7a1da45c9e9cce4a90a174de17c8a8e1742
zeworks <josenogueira95@hotmail.com>
2022-10-30 01:09:36 +0100

    fix load company by id tests

--- src/data/usecases/companies/db-load-company-by-id.spec.ts
+++ src/data/usecases/companies/db-load-company-by-id.spec.ts
@@ -1,11 +1,6 @@
 import { InMemoryCompaniesRepository } from "@/data/protocols/repositories/companies/in-memory-companies-repository";
 import { UuidAdapter } from "@/infra/cryptography/uuid";
-import { ContactsRepository } from "@/infra/db/prisma/repos/contacts-repository";
-import { makeCreateCompanyValidation } from "@/main/factories/controllers/companies/create-company-controller-validation";
-import { makeCreateContactValidation } from "@/main/factories/controllers/contacts/create-contact-controller-validation";
-import { CreateCompanyController } from "@/presentation/controllers/companies/create-company-controller";
 import { expect, test } from "vitest";
-import { DbCreateContact } from "../contacts/db-create-contact";
 import { DbCreateCompany } from "./db-create-company";
 import { DbLoadCompanyById } from "./db-load-company-by-id";
 
@@ -13,18 +8,15 @@ test("Should load company by id with success", async () => {
   const uuidAdapter = new UuidAdapter()
   const companiesRepository = new InMemoryCompaniesRepository()
   const createCompany = new DbCreateCompany(uuidAdapter, companiesRepository);
-  const contactsRepository = new ContactsRepository()
-  const createContact = new DbCreateContact(uuidAdapter, contactsRepository);
-  const createCompanyController = new CreateCompanyController(makeCreateCompanyValidation(), makeCreateContactValidation(), createContact, createCompany);
   const loadCompany = new DbLoadCompanyById(companiesRepository);
 
-  const company = await createCompanyController.execute({
+  const company = await createCompany.create({
     code: "123",
     name: "Company name"
   });
 
-  if (company.data?.id) {
-    const result = await loadCompany.loadById(company.data?.id);
+  if (company?.id) {
+    const result = await loadCompany.loadById(company?.id);
 
     expect(result).not.toBeUndefined();
     expect(result?.code).toEqual("123");
