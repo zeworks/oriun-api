b013969448520efa17b1d117b4081c55423f6ee3
zeworks <josenogueira95@hotmail.com>
2022-10-30 01:10:37 +0100

    companies data db | update + load by code

---
+++ src/data/usecases/companies/db-load-company-by-code.ts
@@ -0,0 +1,17 @@
+import { LoadCompanyByCodeRepository } from "@/data/protocols/repositories/companies/load-company-by-code-repository";
+import { LoadCompanyByCodeUseCase, LoadCompanyByCodeUseCaseFunction } from "@/domain/usecases/companies/load-company-by-code";
+
+export class DbLoadCompanyByCode implements LoadCompanyByCodeUseCase {
+  constructor(
+    private readonly loadCompanyByCodeRepository: LoadCompanyByCodeRepository
+  ) { }
+  
+  loadByCode: LoadCompanyByCodeUseCaseFunction = async (code) => {
+    const result = await this.loadCompanyByCodeRepository.loadByCode(code);
+    
+    if (!result)
+      throw new Error("invalid code");
+    
+    return result;
+  }
+}
---
+++ src/data/usecases/companies/db-update-company.ts
@@ -0,0 +1,19 @@
+import { LoadCompanyByIdRepository } from "@/data/protocols/repositories/companies/load-company-by-id-repository";
+import { UpdateCompanyRepository } from "@/data/protocols/repositories/companies/update-company-repository";
+import { UpdateCompanyUseCase, UpdateCompanyUseCaseFunction } from "@/domain/usecases/companies/update-company";
+
+export class DbUpdateCompany implements UpdateCompanyUseCase {
+  constructor(
+    private readonly loadCompanyByIdRepository: LoadCompanyByIdRepository,
+    private readonly updateCompanyRepository: UpdateCompanyRepository
+  ) { }
+  
+  update: UpdateCompanyUseCaseFunction = async (input) => {
+    const company = await this.loadCompanyByIdRepository.loadById(input.id);
+
+    if (!company)
+      throw new Error("invalid id");
+    
+    return this.updateCompanyRepository.update(input);
+  }
+}
